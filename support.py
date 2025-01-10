ae597c0233a789632399bf7e8f67c8d669892e8d
Mohit Raje <110410869+Mohit-Raje@users.noreply.github.com>
2025-01-10 18:18:51 +0530

    Add files via upload

---
+++ support.py
@@ -0,0 +1,35 @@
+import streamlit as st 
+
+ 
+def create_gemini_api_key():
+    st.header("Create your Own Google Gemin API Key 🔑")
+    st.subheader("Steps:")
+
+    st.markdown("**Step 1 :**")
+    st.write("Go to : https://aistudio.google.com/prompts/new_chat")
+    st.write("accept terms a conditions")
+    st.image("support\image1.png")
+
+    st.markdown("**Step 2:**")
+    st.write("Go to Get API key")
+    st.image("support\image2.png")
+    st.image("support\image3.png")
+
+    st.markdown("**Step 3:**")
+    st.write("Click on Create API Key")
+    st.image("support\image4.png")
+
+    st.markdown("**Step 4 :**")
+    st.write("Select My First Project")
+    st.image("support\image5.png")
+
+    st.markdown("**Step 5:**")
+    st.write("API Key will be created -> Copy the API Key and store it in a file")
+    st.image("support\image6.png")
+
+    st.markdown("**Step 6:**")
+    st.write("Google Gemini API Key Successfully created")
+    st.image("support\image7.png")
+    
+    st.subheader("Refresh the page once the API Key is created to run the application")
+
